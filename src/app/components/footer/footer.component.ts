3b695b84f16f189b79cd54a92272bc53fcefc7f3
DinefGH <112215706+DinefGH@users.noreply.github.com>
2024-11-03 17:04:07 +0100

    translate footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {TranslateModule } from '@ngx-translate/core'; // Import TranslateModule
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
