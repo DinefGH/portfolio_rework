3232497997acc8d5f43cf9385b2e544b3856fa4d
DinefGH <112215706+DinefGH@users.noreply.github.com>
2024-11-15 00:45:34 +0100

    https

--- src/app/api-url.prod.ts
+++ src/app/api-url.prod.ts
@@ -1,2 +1,2 @@
 // api-url.prod.ts
-export const API_URL = 'http://portfolio.server.fabianduerr.com/api/increment-visit/';
+export const API_URL = 'https://portfolio.server.fabianduerr.com/api/increment-visit/';
